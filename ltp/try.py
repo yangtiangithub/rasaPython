51cc5a7fecb123bc68f30bb6b7e84d8494d2a89c
yang <547941595@qq.com>
2018-05-15 20:14:21 +0800

    test pull modify

--- ltp/try.py
+++ ltp/try.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
-import os
-LTP_DATA_DIR = "./ltp_data_v3.4.0"  # ltp模型目录的路径
-pos_model_path = os.path.join(LTP_DATA_DIR, 'pos.model')  # 词性标注模型路径，模型名称为`pos.model`
+# # -*- coding: utf-8 -*-
+# import os
+# LTP_DATA_DIR = "./ltp_data_v3.4.0"  # ltp模型目录的路径
+# pos_model_path = os.path.join(LTP_DATA_DIR, 'pos.model')  # 词性标注模型路径，模型名称为`pos.model`
+#
+# from pyltp import Postagger
+# postagger = Postagger() # 初始化实例
+# postagger.load(pos_model_path)  # 加载模型
+#
+# words = ['元芳', '你', '怎么', '看']  # 分词结果
+# postags = postagger.postag(words)  # 词性标注
+#
+# print ('\t'.join(postags))
+# postagger.release()  # 释放模型
 
-from pyltp import Postagger
-postagger = Postagger() # 初始化实例
-postagger.load(pos_model_path)  # 加载模型
 
-words = ['元芳', '你', '怎么', '看']  # 分词结果
-postags = postagger.postag(words)  # 词性标注
-
-print ('\t'.join(postags))
-postagger.release()  # 释放模型
-
+#add1
 #test git push
+#add 
